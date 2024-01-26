1c802f93745f2c35b196aa2ab43884063728c9ce
michaeladilulo <michaeladilulo@gmail.com>
2024-01-25 20:38:45 -0700

    First riddle heading complete with animations

---
+++ src/components/FirstRiddle/FirstRiddle.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './FirstRiddle.css';
+
+const FirstRiddle = () => {
+    return (
+        <div className="first-riddle-container">
+        <h2 className="first-riddle">
+            <span>I </span>
+            <span>dance </span>
+            <span>in </span>
+            <span>the </span>
+            <span>shadows, </span>
+            <span>born </span>
+            <span>of </span>
+            <span>fire's </span>
+            <span>breath, </span>
+            <span>A </span>
+            <span>realm </span>
+            <span>of </span>
+            <span>torment, </span>
+            <span>harbringer </span>
+            <span>of </span>
+            <span>death. </span>
+            <span>Within </span>
+            <span>the </span>
+            <span>depths, </span>
+            <span>my </span>
+            <span>flames </span>
+            <span>do </span>
+            <span>swirl. </span>
+            <span>Guess </span>
+            <span>my </span>
+            <span>name, </span>
+            <span>I </span>
+            <span>am </span>
+            <span>the </span>
+            <span>_____________.</span>
+            </h2>
+        </div>
+    )
+}
+
+export default FirstRiddle
--- src/pages/initial-page/First.jsx
+++ src/pages/initial-page/First.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import background from '../../assets/infernal.jpg';
+import FirstRiddle from '../../components/FirstRiddle/FirstRiddle'
 import './First.css';
 
 const First = () => {
     return (
         <div className='bg'>
+            <FirstRiddle />
         <img src={background} 
-        style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '100vh', width: '100vw'}} />
+        style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '100vh', width: '100vw', filter: 'brightness(50%)'}} />
         </div>
     )
 }
