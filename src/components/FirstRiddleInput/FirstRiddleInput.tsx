44a8c3c14536992aa3e6365d89fcd058b8100f7b
michaeladilulo <michaeladilulo@gmail.com>
2024-01-25 21:02:57 -0700

    Input component in FR, CSS styles added

---
+++ src/components/FirstRiddleInput/FirstRiddleInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './FirstRiddleInput.css';
+
+const FirstRiddleInput = () => {
+    return (
+        <div className="first-riddle-container">
+        <input className="first-riddle-input" />
+        </div>
+    )
+}
+
+export default FirstRiddleInput
--- src/pages/initial-page/First.jsx
+++ src/pages/initial-page/First.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import background from '../../assets/infernal.jpg';
-import FirstRiddle from '../../components/FirstRiddle/FirstRiddle'
+import FirstRiddle from '../../components/FirstRiddle/FirstRiddle';
+import FirstRiddleInput from '../../components/FirstRiddleInput/FirstRiddleInput';
 import './First.css';
 
 const First = () => {
     return (
         <div className='bg'>
             <FirstRiddle />
+            <FirstRiddleInput />
         <img src={background} 
         style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '100vh', width: '100vw', filter: 'brightness(50%)'}} />
         </div>
