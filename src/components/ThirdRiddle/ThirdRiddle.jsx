d45f08d1a4371c950225b20552d83bd183462cca
michaeladilulo <michaeladilulo@gmail.com>
2024-01-27 07:50:41 -0700

    Image add, CSS files, adds third riddle component

---
+++ src/components/ThirdRiddle/ThirdRiddle.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './ThirdRiddle.css';
+
+const ThirdRiddle = () => {
+    return (
+        <div className="third-riddle-container">
+        <h1 className="third-riddle">
+            <span>I </span>
+            <span>am </span>
+            <span>a </span>
+            <span>haven </span>
+            <span>for </span>
+            <span>tales </span>
+            <span>unfold, </span>
+            <span>filled </span>
+            <span>with </span>
+            <span>stories, </span>
+            <span>both </span>
+            <span>new </span>
+            <span>and </span>
+            <span>old. </span>
+            <span>My </span>
+            <span>walls </span>
+            <span>adorned </span>
+            <span>with </span>
+            <span>paper </span>
+            <span>treasures, </span>
+            <span>where </span>
+            <span>imagination </span>
+            <span>knows </span>
+            <span>no </span>
+            <span>measures. </span>
+            <span>What </span>
+            <span>am </span>
+            <span>I </span>
+            <span>??? </span>
+            </h1>
+        </div>
+    )
+}
+
+export default ThirdRiddle
